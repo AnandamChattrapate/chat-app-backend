6887eec4273f8e2b5c146e135694fcfdfae3d63d
Anandam Chattrapate <23eg105e05@anurag.edu.in>
2026-02-22 23:52:56 +0530

    Update CORS origin to include Netlify URL

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -15,7 +15,7 @@ app.use(exp.json());
 app.use(exp.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "https://chat-app-569.netlify.app" ||process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
 
